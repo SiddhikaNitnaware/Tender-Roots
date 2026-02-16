cdf37cba62fd07f45bec9805ebe0936e9f06eb69
Siddhika Nitnaware <126196759+SiddhikaNitnaware@users.noreply.github.com>
2026-02-17 01:03:32 +0530

    Create constants.py file with game configuration constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+# Game Configuration Constants
+
+# Game settings
+SCREEN_WIDTH = 800  # Width of the game window
+SCREEN_HEIGHT = 600  # Height of the game window
+FPS = 60  # Frames per second
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Game states
+MENU = 'menu'
+PLAYING = 'playing'
+GAME_OVER = 'game_over'
